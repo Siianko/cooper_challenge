38ea44935c2a9ec1664c256adafd6ebf2aa48d2f
your_name <tsiyanko@gmail.com>
2018-08-09 12:42:08 +0200

    finised with backend

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -12,16 +12,30 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
           entry = PerformanceData.last
           expect(entry.data).to eq 'message' => 'Average'
         end
-      end
+    end
     
     describe 'GET /api/v1/performance_data' do
       before do
         5.times { user.performance_data.create(data: { message: 'Average' }) }
       end
+    
 
       it 'returns a collection of performance data' do
         get '/api/v1/performance_data', headers: headers
         expect(response_json['entries'].count).to eq 5
+      end
+
+      it 'returns a collection of performance data belonging to the current user' do
+        get '/api/v1/performance_data', headers: headers
+        current_user_entry = PerformanceData.last
+        expected_response = {
+            'id' => current_user_entry.id, 
+            #'uid' => current_user_entry.email, 
+            #'email' => current_user_entry.email, 
+            'name' => nil, 
+            'nickname' => nil, 
+            'image' => nil
+          }
       end
     end
 end
